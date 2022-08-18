00b8c919a12a31fd72a6ddfa619e38f74013027e
Jurgen Kluft <jurgen@virtuosgames.com>
2022-08-18 16:24:24 +0800

    * adjust package/package.go

--- package/package.go
+++ package/package.go
@@ -1,28 +1,28 @@
-package xcmdline
+package ccmdline
 
 import (
+	cbase "github.com/jurgen-kluft/cbase/package"
 	"github.com/jurgen-kluft/ccode/denv"
-	xbase "github.com/jurgen-kluft/xbase/package"
-	xunittest "github.com/jurgen-kluft/xunittest/package"
+	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
-// GetPackage returns the package object of 'xcmdline'
+// GetPackage returns the package object of 'ccmdline'
 func GetPackage() *denv.Package {
 	// Dependencies
-	unittestpkg := xunittest.GetPackage()
-	basepkg := xbase.GetPackage()
+	unittestpkg := cunittest.GetPackage()
+	basepkg := cbase.GetPackage()
 
-	// The main (xcmdline) package
-	mainpkg := denv.NewPackage("xcmdline")
+	// The main (ccmdline) package
+	mainpkg := denv.NewPackage("ccmdline")
 	mainpkg.AddPackage(unittestpkg)
 	mainpkg.AddPackage(basepkg)
 
-	// 'xcmdline' library
-	mainlib := denv.SetupDefaultCppLibProject("xcmdline", "github.com\\jurgen-kluft\\xcmdline")
+	// 'ccmdline' library
+	mainlib := denv.SetupDefaultCppLibProject("ccmdline", "github.com\\jurgen-kluft\\ccmdline")
 	mainlib.Dependencies = append(mainlib.Dependencies, basepkg.GetMainLib())
 
-	// 'xcmdline' unittest project
-	maintest := denv.SetupDefaultCppTestProject("xcmdline_test", "github.com\\jurgen-kluft\\xcmdline")
+	// 'ccmdline' unittest project
+	maintest := denv.SetupDefaultCppTestProject("ccmdline_test", "github.com\\jurgen-kluft\\ccmdline")
 	maintest.Dependencies = append(maintest.Dependencies, unittestpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, basepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
--- source/test/cpp/test_x_cmdline.cpp
+++ source/test/cpp/test_x_cmdline.cpp
@@ -1,7 +1,7 @@
 #include "cbase/c_target.h"
 #include "cbase/c_runes.h"
 #include "ccmdline/xcmdline.h"
-#include "cunittest/xunittest.h"
+#include "cunittest/cunittest.h"
 
 using namespace ncore;
 
